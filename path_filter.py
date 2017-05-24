7332d5a37edaed968d9955d3ca2b5a1d38473a32
YK-Orfeluna <k030913@kansai-u.ac.jp>
2017-05-24 15:14:16 +0900

    170524

--- path_filter.py
+++ path_filter.py
@@ -5,38 +5,51 @@ p = 1e-12
 u = 1e-6
 farad = [10*p, 100*p, 0.001*u, 0.01*u, 0.1*u, 1*u, 10*u, 0.022*u, 0.033*u, 0.047*u, 0.47*u]
 
+
+capacitor = {
+				100:10*p, 
+				101:100*p, 
+				102:0.001*u, 
+				103:0.01*u, 
+				104:0.1*u, 
+				105:u, 
+				106:10*u, 
+				223:0.022*u, 
+				333:0.033*u, 
+				473:0.047*u, 
+				474:0.47*u
+			}
+
 def filter(fc) :
-	global capacitor, farad
+
+	global capacitor
 
 	t = 1.0 / (fc * 2*pi)
 
-	r = []
-	for i in farad :
-		r.append(str(round(t/i, 3))+"\u03a9")
-
-	for x, i in enumerate(r) :
-		print("%s and %s" %(i, capacitor[x]))
+	for i in capacitor :
+		r = str(round((t / capacitor[i]), 2))
+		print("%s %s and %s" %(r, "\u03a9", i))
+	
 
 def freq(r, c) :
 	global capacitor, farad
 
-	index = capacitor.index(c)
-	c = farad[index]
+	c = capacitor[c]
 
 	fc = 1.0 / (2 * pi * r * c)
 	return fc
 
 if __name__ == "__main__" :
-	print("A frequency(Hz) which you want to cut using HPF or LPF")
-	fc = int(input(">>>"))
+	print("A frequency which you want to cut using HPF or LPF")
+	fc = int(input("Hz >>>"))
 
 	filter(fc)
 
-	print("Which resister(\u03a9) do you use?")
-	r = float(input(">>>"))
+	print("Which resister do you use?")
+	r = float(input("\u03a9 >>>"))
 
 	print("Which capacitor do you use?")
-	c = int(input(">>>"))
+	c = int(input("No. >>>"))
 
 	print("\n%.3f Hz" %freq(r, c))
 
